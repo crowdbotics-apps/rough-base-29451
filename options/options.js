f281e69b5f1d306ccefe423f0f70c69b0f1fd340
Crowdbotics <team@crowdbotics.com>
2021-08-04 14:39:36 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "rough_base_29451Identifier",
+  url: "https://rough_base_29451Identifier.botics.co",
+  api: "https://rough_base_29451Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
